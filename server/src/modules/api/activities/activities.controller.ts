5d0411440efc8e9e88f99d5c9f312fc62f4ccf85
Rock_it_science <wilmc17@gmail.com>
2026-01-19 17:18:30 -0700

    Standardize athleteID and add getActivity endpoint

--- server/src/modules/api/activities/activities.controller.ts
+++ server/src/modules/api/activities/activities.controller.ts
@@ -1,17 +1,18 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { activitiesService } from "./activities.service";
+import { getAthleteID } from "../../../utils/auth-utils";
 
 export const activitiesController = {
 	getActivities: async (request: FastifyRequest, reply: FastifyReply) => {
-		const athleteId = request.cookies.athleteId;
-		if (!athleteId || !parseInt(athleteId)) {
-			reply.status(400).send({ error: "Bad or missing athlete ID" });
-			return;
+		const athleteId = getAthleteID(request);
+		if (!athleteId) {
+			return reply
+				.status(400)
+				.send({ error: "Error logging out, no athlete ID" });
 		}
-		let response;
 		try {
-			response = await activitiesService.getActivities(
-				parseInt(athleteId),
+			const response = await activitiesService.getActivities(
+				athleteId,
 				request.server.db,
 			);
 			reply.status(200).send(response);
@@ -25,21 +26,45 @@ export const activitiesController = {
 		request: FastifyRequest,
 		reply: FastifyReply,
 	) => {
-		const athleteId = request.cookies.athleteId;
-		if (!athleteId || !parseInt(athleteId)) {
+		const athleteId = getAthleteID(request);
+		if (!athleteId) {
 			reply.status(400).send("Bad or missing athlete ID");
 			return;
 		}
 		let response;
 		try {
 			response = await activitiesService.getActivitiesPolylines(
-				parseInt(athleteId),
+				athleteId,
 				request.server.db,
 			);
 			reply.status(200).send(response);
 		} catch (error) {
 			reply.status(500).send({
 				error: `Error fetching activity polylines from app db: ${error}`,
+			});
+		}
+	},
+	getActivity: async (request: FastifyRequest, reply: FastifyReply) => {
+		const { activityId } = request.query as { activityId: number | null };
+		if (!activityId) {
+			reply.status(400).send({ error: "Missing activity ID" });
+			return;
+		}
+		const athleteId = getAthleteID(request);
+		if (!athleteId) {
+			reply.status(400).send({ error: "Bad or missing athlete ID" });
+			return;
+		}
+		let response;
+		try {
+			response = await activitiesService.getActivity(
+				activityId,
+				request.server.db,
+			);
+			reply.status(200).send(response);
+		} catch (error) {
+			reply.status(500).send({
+				error: `Error fetching activities from app db: ${error}`,
 			});
 		}
 	},
