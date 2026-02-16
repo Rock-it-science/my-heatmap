aa6a0a7d578465c2bb6a324fbd9135eaa37e2a6e
Will McFarland <wilmc17@gmail.com>
2026-02-16 01:14:05 -0700

    frontend server URL dynamic config

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,7 +7,9 @@ export function HomePage() {
 
 	const cta = {
 		text: isLoggedIn ? "Dashboard" : "Connect Account",
-		link: isLoggedIn ? "/heatmap" : "http://localhost:8085/api/user/auth",
+		link: isLoggedIn
+			? "/heatmap"
+			: `${import.meta.env.VITE_SERVER_URL}/api/user/auth`,
 	};
 
 	const onClickCTA = () => {
