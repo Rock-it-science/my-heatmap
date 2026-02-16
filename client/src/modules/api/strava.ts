6c3dcf6b8f3b84b64ae1e4532cbd4baaaaf5e748
Will McFarland <wilmc17@gmail.com>
2026-02-16 11:46:36 -0700

    clean up frontend API call code

--- client/src/modules/api/strava.ts
+++ client/src/modules/api/strava.ts
@@ -1,6 +1,68 @@
 import { StravaActivity, StravaActivityResponse } from "@/types";
 import polyline from "@mapbox/polyline";
-import { filterActivitiesEmptyMap } from "../map/activities/utils";
+
+function getLocalActivitiesIfAvailable(): StravaActivity[] | false {
+	const localActivities = localStorage.getItem("stravaActivities");
+	if (localActivities) {
+		const parsedActivities = JSON.parse(localActivities);
+		if (parsedActivities) {
+			console.log("Using cached activities");
+			return parsedActivities;
+		}
+	}
+	return false;
+}
+
+function storeActivities(filteredActivities: StravaActivity[]) {
+	try {
+		localStorage.setItem(
+			"stravaActivities",
+			JSON.stringify(filteredActivities),
+		);
+		return;
+	} catch (error) {
+		throw Error(`Error storing activities in local storage: ${error}`);
+	}
+}
+
+async function fetchStravaActivitiesFromAPI(): Promise<
+	StravaActivityResponse[]
+> {
+	console.log("Fetching activities from API");
+	try {
+		const response = await fetch("/api/activities");
+		const stravaActivitiesResponse =
+			(await response.json()) as StravaActivityResponse[];
+		return stravaActivitiesResponse;
+	} catch (error) {
+		throw Error(`Error fetching activities from API: ${error}`);
+	}
+}
+
+function decodePolylinePoints(
+	stravaActivitiesResponse: StravaActivityResponse[],
+): (StravaActivityResponse & { polylinePoints?: [number, number][] })[] {
+	const stravaActivities = stravaActivitiesResponse.map((activity) => {
+		const polylinePoints = activity.mapPolyline
+			? polyline.decode(activity.mapPolyline)
+			: undefined;
+		return {
+			polylinePoints,
+			...activity,
+		};
+	});
+	return stravaActivities;
+}
+
+function filterActivitiesEmptyMap(
+	activities: (StravaActivityResponse & {
+		polylinePoints?: [number, number][];
+	})[],
+): StravaActivity[] {
+	return activities.filter(
+		(activity) => activity.polylinePoints !== undefined,
+	) as StravaActivity[];
+}
 
 /**
  * Check if strava activities are in local storage.
@@ -13,31 +75,18 @@ import { filterActivitiesEmptyMap } from "../map/activities/utils";
  * @returns list of activity objects
  */
 export async function fetchStravaActivities(): Promise<StravaActivity[]> {
-	const localActivities = localStorage.getItem("stravaActivities");
+	const localActivities = getLocalActivitiesIfAvailable();
 	if (localActivities) {
-		const parsedActivities = JSON.parse(localActivities);
-		if (parsedActivities) {
-			console.log("Using cached activities");
-			return parsedActivities;
-		}
+		return localActivities;
 	}
-	console.log("Fetching activities from API");
-	const response = await fetch("/api/activities");
-	const stravaActivitiesResponse =
-		(await response.json()) as StravaActivityResponse[];
-	const stravaActivities = stravaActivitiesResponse.map((activity) => {
-		const polylinePoints = activity.mapPolyline
-			? polyline.decode(activity.mapPolyline)
-			: undefined;
-		return {
-			polylinePoints,
-			...activity,
-		};
-	});
-	const filteredActivities = filterActivitiesEmptyMap(stravaActivities);
-	localStorage.setItem(
-		"stravaActivities",
-		JSON.stringify(filteredActivities),
+
+	const activitiesResponse = await fetchStravaActivitiesFromAPI();
+	const activitiesDecodedPoints = decodePolylinePoints(activitiesResponse);
+	const activitiesFiltered = filterActivitiesEmptyMap(
+		activitiesDecodedPoints,
 	);
-	return filteredActivities;
+
+	storeActivities(activitiesFiltered);
+
+	return activitiesFiltered;
 }
