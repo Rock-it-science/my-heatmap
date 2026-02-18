e59edd95f604acb1fc87361ac1694283afedbf53
Will McFarland <wilmc17@gmail.com>
2026-02-17 21:11:21 -0700

    Use frontend url in strava oauth url

--- server/src/types/constants.ts
+++ server/src/types/constants.ts
@@ -1,2 +1,1 @@
-export const STRAVA_OAUTH_URL =
-	"https://www.strava.com/oauth/authorize?client_id=175179&response_type=code&redirect_uri=http://localhost:8085/api/auth/callback&approval_prompt=force&scope=read,activity:read";
+export const STRAVA_OAUTH_URL = `https://www.strava.com/oauth/authorize?client_id=175179&response_type=code&redirect_uri=${process.env.FRONTEND_URL}/api/auth/callback&approval_prompt=force&scope=read,activity:read`;
