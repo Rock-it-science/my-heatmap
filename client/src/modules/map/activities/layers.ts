9b36378af461f5b8e97c40e9c985b292cbb20489
Will <wilmc17@gmail.com>
2026-03-21 00:11:16 -0600

    add mouse-over events to activity lines

--- client/src/modules/map/activities/layers.ts
+++ client/src/modules/map/activities/layers.ts
@@ -2,6 +2,8 @@ import { StravaActivity } from "shared/index";
 import L from "../leaflet-setup";
 import { createActivityPopup } from "./activity-popup";
 
+const DEFAULT_OPACITY = 0.2;
+
 export function createHeatLayer(activityPolyLines: StravaActivity[]) {
 	let allCoords: [number, number, number][] = [];
 	for (const activity of activityPolyLines) {
@@ -21,19 +23,43 @@ export function createActivityLinesLayerGroup(
 	activityPolyLines: StravaActivity[],
 ): L.LayerGroup<L.Polyline & { sportType: string }> {
 	const activityLineLayers = activityPolyLines.map((activityPolyline) => {
-		const activityLineLayer = L.polyline(activityPolyline.polylinePoints, {
+		const activityLineLayer: L.Polyline & { sportType?: string } = L.polyline(activityPolyline.polylinePoints, {
 			color: activityPolyline.sportTypeColour,
-			opacity: 0.2,
+			opacity: DEFAULT_OPACITY,
 		});
-		(activityLineLayer as any).sportType = activityPolyline.sportType;
+		activityLineLayer.sportType = activityPolyline.sportType;
 		activityLineLayer.bindPopup(
 			() => createActivityPopup(activityPolyline),
 			{
 				minWidth: 200,
 			},
 		);
+
+		setActivityLineEvents(activityLineLayer);
+
 		return activityLineLayer;
 	});
 	const activityLinesLayerGroup = L.layerGroup(activityLineLayers);
 	return activityLinesLayerGroup;
+}
+
+/**
+ * Set activity line event listeners to raise opacity and weight of lines when they are hovered over, or when popup is open.
+ * Mouse-out should not clear changes if popup is open (activity line should stay boldened as long as popup is open)
+ */
+function setActivityLineEvents(activityLineLayer: L.Polyline & { sportType?: string }) {
+	let isPopupOpen = false;
+	activityLineLayer.on("popupopen", (ev) => {
+		isPopupOpen = true;
+		(ev.target as L.Polyline).setStyle({ weight: 6, opacity: 1 })
+	}
+	);
+	activityLineLayer.on("popupclose", (ev) => {
+		isPopupOpen = false;
+		(ev.target as L.Polyline).setStyle({ weight: 3, opacity: DEFAULT_OPACITY })
+	}
+	);
+
+	activityLineLayer.on("mouseover", (ev) => { if (!isPopupOpen) (ev.target as L.Polyline).setStyle({ weight: 6, opacity: 1 }) });
+	activityLineLayer.on("mouseout", (ev) => { if (!isPopupOpen) (ev.target as L.Polyline).setStyle({ weight: 3, opacity: DEFAULT_OPACITY }) });
 }
